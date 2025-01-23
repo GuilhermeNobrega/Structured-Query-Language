2c34bb3c10201ddd8277ac084940fbaf8b588a63
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-01-23 16:50:46 -0300

    Update Consulta3.sql

--- Consulta3.sql
+++ Consulta3.sql
@@ -1,10 +1,12 @@
 -- Conte quantos pedidos cada cliente fez, exibindo o nome e o total de pedidos
 SELECT Customers.customer_id as id_usuario,
   first_name as nome,
-  item as item ,
-  amount as quant_total
+  item as item,
+  amount as quant_total,
+  COUNT(Orders.order_id)
 
 FROM Customers
 INNER JOIN Orders
 	ON Customers.customer_id = Orders.customer_id
-	ORDER BY first_name ASC;
+GROUP BY Customers.customer_id, first_name
+ORDER BY first_name ASC;
