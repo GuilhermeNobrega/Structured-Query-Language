3d3114f4189afa49ae2987808e522d6a42953f68
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-01-23 10:21:09 -0300

    Update Consulta5.sql

--- Consulta5.sql
+++ Consulta5.sql
@@ -1,1 +1,14 @@
+-- Liste os clientes que possuem pedidos, mas nenhum envio foi feito para eles ainda.
 
+SELECT Customers.customer_id,
+  first_name as nome,
+  last_name,
+  item as item, 
+  status as status
+  
+FROM Customers
+  INNER JOIN Orders
+  ON Customers.customer_id = Orders.customer_id
+  INNER JOIN Shippings
+  ON Customers.customer_id = Shippings.customer
+WHERE status like 'P%NG';
