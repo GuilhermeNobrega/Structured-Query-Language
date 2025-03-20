cedd654f0b0e0339223a9ccbf5e644be600b446c
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-03-20 10:37:44 -0300

    Update LeftJoin1.sql

--- Left/LeftJoin1.sql
+++ Left/LeftJoin1.sql
@@ -3,3 +3,9 @@ From Customers
 LEFT JOIN Orders -- Inclui registros da tabela esquerda com registros correspondentes na direita
 ON Customers.customer_id = Orders.customer_id
 WHERE Customers.customer_id NOT IN (SELECT Orders.customer_id FROM Orders);
+-- OU
+
+
+SELECT customer_id, first_name, last_name
+FROM Customers
+WHERE customer_id NOT IN (SELECT customer_id FROM Orders);
