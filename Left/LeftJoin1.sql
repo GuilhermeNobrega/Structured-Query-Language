3dcee20f2d9f6638924a7af1adc549465b2429a4
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-03-20 10:38:43 -0300

    Update LeftJoin1.sql

--- Left/LeftJoin1.sql
+++ Left/LeftJoin1.sql
@@ -3,7 +3,7 @@ From Customers
 LEFT JOIN Orders -- Inclui registros da tabela esquerda com registros correspondentes na direita
 ON Customers.customer_id = Orders.customer_id
 WHERE Customers.customer_id NOT IN (SELECT Orders.customer_id FROM Orders);
--- OU
+-- OU NOT IN (SELECT ...) pode dar problema se houver NULL no subquery.
 SELECT Customers.customer_id, Customers.first_name, Customers.last_name
 FROM Customers
 LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
