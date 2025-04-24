1f13b6668b03c22f9bcf9266dacac4284af0f6b0
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-04-24 09:20:19 -0300

    Update LeftJoin1.sql

--- Left/LeftJoin1.sql
+++ Left/LeftJoin1.sql
@@ -4,29 +4,11 @@ LEFT JOIN Orders -- Inclui registros da tabela esquerda com registros correspond
 ON Customers.customer_id = Orders.customer_id
 WHERE Customers.customer_id NOT IN (SELECT Orders.customer_id FROM Orders);
 
+
 -- OU NOT IN (SELECT ...) pode dar problema se houver NULL no subquery.
+
 
 SELECT Customers.customer_id, Customers.first_name, Customers.last_name
 FROM Customers
 LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
 WHERE Orders.customer_id IS NULL; -- basicamente aqui ele seleciona usuários de Customers que não estão contidos em Orders
-
--- Outro possível jeito, que deu certo (Deixa item null quando não tem)
-
-SELECT C.customer_id, C.first_name, O.item
-FROM Customers C
-LEFT JOIN Orders O
-ON C.customer_id = O.customer_id
-WHERE O.customer_id IS NULL OR O.customer_id IS NOT NULL;
--- OU 
-
-SELECT C.customer_id, C.first_name, O.item
-FROM Customers C
-LEFT JOIN Orders O
-ON C.customer_id = O.customer_id;
-
-
-
-SELECT customer_id, first_name, last_name
-FROM Customers
-WHERE customer_id NOT IN (SELECT customer_id FROM Orders);
